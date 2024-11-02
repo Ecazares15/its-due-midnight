af22fc5d8a2ba54e2e547ab66ccb8410e3da58ab
Eddie <Ecazares15@utexas.edu>
2024-11-02 14:13:36 -0500

    Added some more stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import { Input } from "antd";
+import { Input, Button } from "antd";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <div>
-        What is due at Midnight?
+      <div className="text-7xl">What is due at Midnight?</div>
+      <div className="text-3xl">
+        Enter a task and we will provide a breakdown for you :D
       </div>
-      <Input className="w-1/2" />
+      <Input className="w-1/4 m-3" placeholder="Enter a task..." />
+      <Button type="primary"> LOCK IN TIME </Button>
     </div>
   );
 }
